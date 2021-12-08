9c00bbbe667b59ec99637e274319b5fdbeb1ac70
89wozniak@gmail.com <89wozniak@gmail.com>
2021-12-08 23:03:12 +0100

    Finish!

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:70] + "..."
+
+    def __str__(self):
+        return self.title
